4217e5258fdb1e2f47bd9430fad298da03beaf92
joyastockwell <stockwja@rose-hulman.edu>
2018-06-22 16:15:01 -0400

    New stuff for Matthew

--- src/main/kotlin/ui/MyApp.kt
+++ src/main/kotlin/ui/MyApp.kt
@@ -1,4 +1,5 @@
 import javafx.beans.property.SimpleStringProperty
+import javafx.beans.value.ChangeListener
 import javafx.collections.FXCollections
 import tornadofx.*
 class MyApp: App(MyView::class)
@@ -18,11 +19,45 @@ class MyView : View() {
         //(map does the same operation to each elem of the list, storing the result in a new list)
         val langsnames = langsdata?.map { it.title };
         //we make a drop-down menu out of a combobox and tell the combobox to store the selection of the user in selected
-        val selected = SimpleStringProperty();
-        combobox(selected, langsnames);
+        var selectedLang = SimpleStringProperty();
+        val langBox = combobox(selectedLang, langsnames);
+        val selectedBook = SimpleStringProperty();
+        var bookBox = combobox(selectedBook,listOf("Select a language"));
+        val selectedChap = SimpleStringProperty();
+        var chapBox = combobox(selectedChap, listOf("Select a book"));
+
+        selectedLang.addListener{observable, oldValue, newValue ->
+            println("hi")
+        }
+        selectedLang.onChange{ println("hello") }
+        langBox.itemsProperty().onChange { println("hey") }
+        langBox.addEventHandler()
+
+
         //make a submit button that tells the program when to act upon what is stored in submit
+        /*
         var myLang: String;
-        button("SUBMIT") {
+
+        button("SUBMIT LANG") {
+            action {
+                if (selectedLang.value == null) {
+                    println("No selection; defaults to Eng")
+                    myLang = "English";
+                } else {
+                    myLang = selectedLang.value;
+                }
+                var metaDataMyLang: LanguageMetadata?;
+                //gets first language in list langsdata that has name selected
+                metaDataMyLang = langsdata?.filter { it.title == myLang }?.get(0);
+                //holds bible data:  has ulb's data
+                var bible = metaDataMyLang?.bibles?.get("ulb");
+                if(bible != null) {
+                    println(bible.books);
+                }
+            }
+        }
+        /*
+        button("SUBMIT BOOK") {
             action {
                 if (selected.value == null) {
                     println("No selection; defaults to Eng")
@@ -40,17 +75,28 @@ class MyView : View() {
                 }
             }
         }
-
-//                books = bible.books[ind].title
-
-                /*
-                combobox<String> {
-                    items = FXCollections.observableList(langsnames);
+        button("SUBMIT CHAPTER") {
+            action {
+                if (selected.value == null) {
+                    println("No selection; defaults to Eng")
+                    myLang = "English";
+                } else {
+                    myLang = selected.value;
                 }
-                */
-
+                var metaDataMyLang: LanguageMetadata?;
+                //gets first language in list langsdata that has name selected
+                metaDataMyLang = langsdata?.filter { it.title == myLang }?.get(0);
+                //holds bible data:  has ulb's data
+                var bible = metaDataMyLang?.bibles?.get("ulb");
+                if(bible != null) {
+                    println(bible.books);
+                }
+            }
         }
+*/*/
+
     }
+}
 
 
 class MyController: Controller() {
