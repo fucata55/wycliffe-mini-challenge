f1f900bbb9c67998a12d0f0f0e575f246b81c87b
joyastockwell <stockwja@rose-hulman.edu>
2018-06-22 13:23:54 -0400

    Gets books in ulb

--- src/main/kotlin/ui/MyApp.kt
+++ src/main/kotlin/ui/MyApp.kt
@@ -1,6 +1,7 @@
 import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections
 import javafx.collections.ObservableList
+import org.intellij.lang.annotations.Language
 import tornadofx.*
 class MyApp: App(MyView::class)
 
@@ -29,19 +30,36 @@ class MyView : View() {
                 //map does the same operation to each elem of the list, storing the result in a new list
                 val langsnames = langsdata.map { it.title };
                 //we make a drop-down menu out of a combobox and tell the combobox to store the selection of the user in selected
-                val selected = SimpleStringProperty()
+                val selected = SimpleStringProperty();
                 combobox(selected, langsnames);
                 //make a submit button that tells the program when to act upon what is stored in submit
+                var myLang: String;
                 button("SUBMIT") {
                     action {
-                        if(selected.value == null){
-                            println("SELECT A LANG")
+                        if (selected.value == null) {
+                            println("No selection; defaults to Eng")
+                            myLang = "English";
+                        } else {
+                            myLang = selected.value;
                         }
-                        else {
-                            println(selected.value);
+                        var metaDataMyLang: LanguageMetadata;
+                        //gets first language in list langsdata that has name selected
+                        metaDataMyLang = langsdata.filter { it.title == myLang }[0];
+                        //holds bible data:  has ulb's data
+                        var bible = metaDataMyLang.bibles["ulb"];
+                        if(bible != null) {
+                            println(bible.books);
                         }
                     }
                 }
+
+//                books = bible.books[ind].title
+
+                //prints the books available in the ulb bible in the selected language
+                //gets language data from language name
+
+
+
                 /*
                 combobox<String> {
                     items = FXCollections.observableList(langsnames);
