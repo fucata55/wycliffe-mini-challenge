add6bcc3555c16801f3475664a9ad4e7ea51a2a1
Matthew Russell <matthew@evlabsapps.com>
2018-06-22 16:11:27 -0400

    Added RxKotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,43 +1,23 @@
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import io.reactivex.Observable
+import io.reactivex.rxjavafx.schedulers.JavaFxScheduler
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
     // create a door43 object
     val door43 = Door43()
 
     // grab the catalog
-    val catalog = door43.fetchCatalog()
-
-    // use the catalog
-    if (catalog != null) {
-        val languages = catalog.getLanguageList() // gets a list of the available languages
-
-        // each language object has information like the identifier, title, and list of available Bibles
-        println("Language Sample: " + languages[0].title + " (" + languages[0].identifier + ") \n")
-
-        // alternatively, you can directly get all the Bibles for a language if you
-        // know the identifier/slug
-        val bibles = catalog.getBiblesForLanguage("en")
-
-        // once you have the Bible metadata from the catalog, you can use it to grab all the chapters in a book
-        if (bibles != null) {
-            // get book information
-            val bookTitle = bibles[0].books[0].title
-            val bookIdentifier = bibles[0].books[0].identifier
-
-
-            // get the book
-            val book = door43.getBook(bibles[0], bookIdentifier)
-
-            // get the chapter text
-            if (book != null) {
-                val chapter = book.chapters[0]
-                // print the chapter information
-                println(bookTitle + " " + chapter.number)
-                println(chapter.text)
+    door43.fetchCatalog()
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.newThread())
+            .subscribe {
+                for (language in it.getLanguageList()) {
+                    println(language.title)
+                }
             }
-        }
-    }
+
+    TimeUnit.SECONDS.sleep(10)
 }
 
 
