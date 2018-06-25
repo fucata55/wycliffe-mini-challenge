f2e2ef728ac88df83a01de0c9603f7262c1f9b0f
joyastockwell <stockwja@rose-hulman.edu>
2018-06-25 09:30:19 -0400

    More comments

--- src/main/kotlin/retrofit/Door43ApiService.kt
+++ src/main/kotlin/retrofit/Door43ApiService.kt
@@ -5,6 +5,7 @@ import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Url
 
+//dagger uses retrofit to create the api service, which is used to make the Door43 objects
 // this is the information Retrofit needs to create the API service
 // it tells Retrofit where the API calls should go and what data objects should be returned
 interface Door43ApiService {
