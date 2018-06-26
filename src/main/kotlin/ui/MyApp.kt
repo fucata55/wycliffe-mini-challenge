359be28699dde3add484a00192106c7e82a69e98
fucata <sliong325@gmail.com>
2018-06-25 22:41:52 -0400

    Try download noto sans with multiple languages

--- src/main/kotlin/ui/MyApp.kt
+++ src/main/kotlin/ui/MyApp.kt
@@ -291,11 +291,13 @@ class MyView : View() {
                 "<html>\n" +
                         "   <head>\n" +
                         "       <style>\n" +
-                        "           @font-face {\n" +
-                        "               font-family: NotoSans\n" +
-                        "               src:url(../../resources/NotoSans-Regular.ttf)\n" +
+                        "           @font-face {\n"+
+                        "               font-family: 'Noto Sans';\n"+
+                        "               font-style: normal;\n"+
+                        "               font-weight: 400;\n"+
+                        "               url('../../resources/noto-sans-v7-greek-ext_latin-ext_cyrillic-ext_vietnamese_greek_latin_devanagari_cyrillic-regular.ttf') format('truetype');\n" +
                         "           }\n" +
-                        "           html {\n" +
+                        "           * {\n" +
                         "               font-family: 'Noto Sans', sans-serif;\n" +
                         "           }\n" +
                         "           body {\n" +
